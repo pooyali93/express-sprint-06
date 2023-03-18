4b7ea4ea70649f3494f6f9735807c53d1e7a83c3
pooyali93 <k2049963@gmail.com>
2023-03-18 00:55:53 +0000

    Update bookings-schema.js

--- validator/bookings-schema.js
+++ validator/bookings-schema.js
@@ -2,9 +2,10 @@ import joi from 'joi'
 
 const schema = {};
 
-schema.mutableFields = ['VEHICLE_ID', 'EMP_ID', 'CUST_ID', 'DATEBOOKED'];
+schema.mutableFields = ['BOOKING_ID','VEHICLE_ID', 'EMP_ID', 'CUST_ID', 'DATEBOOKED'];
 
 schema.recordSchema = joi.object({
+  BOOKING_ID: joi.number().integer(),
   VEHICLE_ID: joi.number().integer(),
   EMP_ID: joi.number().integer(),
   CUST_ID: joi.number().integer(),
