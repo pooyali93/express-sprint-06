6861b50a559bb1b1c0e3ef9e66c04f78a642ce15
pooyali93 <106811535+pooyali93@users.noreply.github.com>
2023-05-03 01:48:21 +0100

    Feedback (#3)
    * feedback endpoint
    
    * Update
    
    * Update bookings-schema.js
    
    * Final Commit
    
    * Final Commit after merg faild

--- validator/bookings-schema.js
+++ validator/bookings-schema.js
@@ -5,13 +5,13 @@ const schema = {};
 schema.mutableFields = ['VEHICLE_ID', 'EMP_ID', 'CUST_ID', 'DATEBOOKED'];
 
 schema.recordSchema = joi.object({
+  BOOKING_ID: joi.number().integer(),
   VEHICLE_ID: joi.number().integer(),
   EMP_ID: joi.number().integer(),
   CUST_ID: joi.number().integer(),
   DATEBOOKED: joi.date()
 
 }).required().unknown(true);
-
 
 
 
